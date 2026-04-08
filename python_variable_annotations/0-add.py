cd67d081650b4ede9381a93d7b93810debfbbd0a
Pawnee <pawneedefize@hotmail.com>
2026-04-08 07:49:16 +0000

    feat: Module that use type-annotated addition function

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module that use type-annotated addition function"""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats a and b"""
+    return a + b
