5c85df5d33451a2cbb6425f50e39e29e9f4b680d
Pawnee <pawneedefize@hotmail.com>
2026-04-09 08:32:29 +0000

    feat: Module that use type-annotated async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine generator"""
     for _ in range(10):
         await asyncio.sleep(1)
