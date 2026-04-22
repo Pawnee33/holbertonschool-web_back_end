8a24e60ebd473cdfe6f5905060d40596226b3919
Pawnee <pawneedefize@hotmail.com>
2026-04-22 10:04:13 +0200

    feat:Module that changes all topics of a school document based on the name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Module that changes all topics of a school
+document based on the name'''
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates topics"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
