a4ed705ae17f9e67b9a4c4ccf76bb77e3cd2139c
Pawnee <pawneedefize@hotmail.com>
2026-05-05 20:22:44 +0200

    feat: manipulations des données avec filer, startWith, map, slice, join

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+    if (typeof startString !== 'string' || startString === '') {
+        return ''
+    }
+    return [...set]
+  .filter((word) => word.startsWith(startString))
+  .map((word) => word.slice(startString.length))
+  .join('-');
+}
