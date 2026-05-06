159a258ddd97d6607a2cac4a8b46f3adfdaf6be1
Pawnee <pawneedefize@hotmail.com>
2026-05-06 15:50:52 +0200

    feat: Promise.allSettled

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,4 +3,5 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results)
 }
