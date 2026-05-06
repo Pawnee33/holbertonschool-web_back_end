d683e85c08134a0c70610b5428198f99ac3bffa8
Pawnee <pawneedefize@hotmail.com>
2026-05-06 11:41:25 +0200

    feat: consommer une promesse

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
     return promise
-    .then((value) => {
-        return ({status: 200, body: 'Success' });
+    .then(() => {
+        return {status: 200, body: 'Success' };
     })
-    .catch((error) => {
+    .catch(() => {
         return new Error();
     })
     .finally(() => {
