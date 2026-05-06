1f4393110db2a50d2ed5163156933ba989125e01
Pawnee <pawneedefize@hotmail.com>
2026-05-06 14:37:59 +0200

    feat: Chaîner des promesse

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -6,6 +6,6 @@ export default function handleProfileSignup() {
             console.log(`${promise[0].body} ${promise[1].firstName} ${promise[1].lastName}`)
         })
         .catch((error) => {
-             console.error("Signup system offline")
+             console.log("Signup system offline")
         }) 
 }
