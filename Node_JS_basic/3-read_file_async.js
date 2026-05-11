08d8f5d8b68ceea5f074476d7bb71888003ccfe3
Pawnee <pawneedefize@hotmail.com>
2026-05-11 17:22:22 +0200

    feat: Read file async

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf-8', (error, data) => {
+            if (error) {
+                reject(new Error('Cannot load the database'));
+            } else {
+                const splitlines = data.split('\n');
+                const slicedata = splitlines.slice(1).filter(splitline => splitline !=='');
+                console.log('Number of students: ' + slicedata.length);
+
+                const students = {};
+                slicedata.forEach(splitline => {
+                    const columns = splitline.split(',');
+                    const firstname = columns[0];
+                    const field = columns[3]
+                    if (!students[field]) {
+                        students[field] = [];
+                    }
+                    students[field].push(firstname)
+                });
+                
+                Object.keys(students).forEach(field => {
+                    console.log('Number of students in ' + field + ': ' + students[field].length + '. List: ' + students[field].join(', '));
+                })
+                resolve();
+            }
+        });
+    });
+}
+
+module.exports = countStudents;
