15a4153bdb5566be3b979116b94fd8bb57185d6e
Pawnee <pawneedefize@hotmail.com>
2026-05-13 11:23:39 +0200

    feat: implement HTTP server with routing

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,31 +1,53 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
-const app = express();
 const database = process.argv[2];
+const app = express();
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const splitlines = data.split('\n');
+        const slicedata = splitlines.slice(1).filter((splitline) => splitline !== '');
+        let resultat = `Number of students: ${slicedata.length}\n`;
+
+        const students = {};
+        slicedata.forEach((splitline) => {
+          const columns = splitline.split(',');
+          const firstname = columns[0];
+          const field = columns[3];
+          if (!students[field]) {
+            students[field] = [];
+          }
+          students[field].push(firstname);
+        });
+
+        Object.keys(students).forEach((field) => {
+          resultat += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+        });
+        resolve(resultat.trimEnd());
+      }
+    });
+  });
+}
 
 app.get('/', (request, response) => {
   response.status(200).send('Hello Holberton School!');
 });
 app.get('/students', (request, response) => {
-  const lines = ['This is the list of our students'];
-  const countStudentsLog = console.log;
-
-  console.log = (...args) => {
-    lines.push(args.join(' '));
-  };
-  countStudents(process.argv[2])
-    .then(() => {
-      console.log = countStudentsLog;
-      response.status(200).send(lines.join('\n'));
+  countStudents(database)
+    .then((resultat) => {
+      response.set('Content-Type', 'text/plain');
+      response.status(200).send(`This is the list of our students\n${resultat}`);
     })
-    .catch((error) => {
-      console.log = countStudentsLog;
-      response.status(500).send(error.message);
+    .catch(() => {
+      response.set('Content-Type', 'text/plain');
+      response.status(500).send('This is the list of our students\nCannot load the database');
     });
 });
 const port = 1245;
-app.listen(port, () => {
-  console.log(`Server up and running on port ${port}`);
-});
+app.listen(port);
 module.exports = app;
