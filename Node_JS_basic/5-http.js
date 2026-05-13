07e49c908a17afefbae84beabe2d89f8fdebf5c3
Pawnee <pawneedefize@hotmail.com>
2026-05-13 10:58:08 +0200

    fix: réécriture de la fonction countStudents

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,5 +1,38 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+
+const database = process.argv[2];
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const splitlines = data.split('\n');
+        const slicedata = splitlines.slice(1).filter((splitline) => splitline !== '');
+        let resultat = `Number of students: ${slicedata.length}\n`;
+
+        const students = {};
+        slicedata.forEach((splitline) => {
+          const columns = splitline.split(',');
+          const firstname = columns[0];
+          const field = columns[3];
+          if (!students[field]) {
+            students[field] = [];
+          }
+          students[field].push(firstname);
+        });
+
+        Object.keys(students).forEach((field) => {
+          resultat += `Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}\n`;
+        });
+        resolve(resultat.trimEnd());
+      }
+    });
+  });
+}
+
 
 const app = http.createServer((request, response) => {
   if (request.url === '/') {
@@ -7,19 +40,12 @@ const app = http.createServer((request, response) => {
     response.end('Hello Holberton School!');
   } else if (request.url === '/students') {
     response.writeHead(200, { 'content-type': 'text/plain' });
-    const lines = ['This is the list of our students'];
-    const countStudentsLog = console.log;
-    console.log = (...args) => {
-      lines.push(args.join(' '));
-    };
     countStudents(process.argv[2])
-      .then(() => {
-        console.log = countStudentsLog;
-        response.end(lines.join('\n'));
+      .then((resultat) => {
+        response.end(`This is the list of our students\n${resultat}`);
       })
-      .catch((error) => {
-        console.log = countStudentsLog;
-        response.end(error.message);
+      .catch(() => {
+        response.end('Cannot load the database');
       });
   }
 });
