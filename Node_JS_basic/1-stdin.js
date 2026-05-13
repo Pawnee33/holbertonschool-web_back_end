abae236f8fedf77474aabd79de91b25c83115e5a
Pawnee <pawneedefize@hotmail.com>
2026-05-13 10:10:25 +0200

    fix: utilise process.stdout.write pour la sortie

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (entry) => {
-  const name = entry.trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${entry}`);
 });
 
 process.stdin.on('end', () => {
