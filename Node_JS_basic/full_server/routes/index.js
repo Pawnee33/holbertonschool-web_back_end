a770cd651d5dfd6e6a05434cf2f1b74e7b463cd1
Pawnee <pawneedefize@hotmail.com>
2026-05-13 18:53:24 +0200

    feat: construction d'un server nodeJS avec express

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-export default express.Router()
-  .get('/', AppController.getHomepage)
-  .get('/students', StudentsController.getAllStudents)
-  .get('/students/:major', StudentsController.getAllStudentsByMajor);
+const router = express.Router();
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+export default router;
--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -2,10 +2,8 @@ import express from 'express';
 import router from './routes/index';
 
 const app = express();
-const port = 1245;
 
-app.use(router)
-
-app.listen(port);
+app.use(router);
+app.listen(1245);
 
 export default app;
