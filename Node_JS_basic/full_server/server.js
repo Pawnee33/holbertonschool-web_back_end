95604288aba79ce53766e14c88128ff9833193eb
Pawnee <pawneedefize@hotmail.com>
2026-05-13 16:03:29 +0200

    feat: construction d'un server nodeJS avec express

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import router from './routes/index';
 
 const app = express();
+const port = 1245;
 
 app.use(router)
 
-const port = 1245;
 app.listen(port);
 
 export default app;
