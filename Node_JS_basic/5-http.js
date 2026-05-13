f08344013bfd5422ddebe2ad74a6fc8e4b3a8513
Pawnee <pawneedefize@hotmail.com>
2026-05-13 10:40:51 +0200

    fix: modifie avec if et else

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,37 +2,27 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((request, response) => {
-  switch (request.url) {
-    case '/': {
-      response.writeHead(200, { 'content-type': 'text/plain' });
-      response.end('Hello Holberton School!');
-      break;
-    }
-    case '/students': {
-      response.writeHead(200, { 'content-type': 'text/plain' });
-      const lines = ['This is the list of our students'];
-      const countStudentsLog = console.log;
-
-      console.log = (...args) => {
-        lines.push(args.join(' '));
-      };
-      countStudents(process.argv[2])
-        .then(() => {
-          console.log = countStudentsLog;
-          response.end(lines.join('\n'));
-        })
-        .catch((error) => {
-          response.end(error.message);
-        });
-      break;
-    }
-    default: {
-      response.writeHead(404, { 'content-type': 'text/plain' });
-      response.end('not found');
-      break;
-    }
+  if (request.url === '/') {
+    response.writeHead(200, { 'content-type': 'text/plain' });
+    response.end('Hello Holberton School!');
+  } else if (request.url === '/students') {
+    response.writeHead(200, { 'content-type': 'text/plain' });
+    const lines = ['This is the list of our students'];
+    const countStudentsLog = console.log;
+    console.log = (...args) => {
+      lines.push(args.join(' '));
+    };
+    countStudents(process.argv[2])
+      .then(() => {
+        console.log = countStudentsLog;
+        response.end(lines.join('\n'));
+      })
+      .catch((error) => {
+        console.log = countStudentsLog;
+        response.end(error.message);
+      });
   }
 });
-const port = 1245;
-app.listen(port);
+
+app.listen(1245);
 module.exports = app;
